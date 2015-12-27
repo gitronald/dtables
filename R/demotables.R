d0514f71ffb8cd3eca8c788f2318d188d471f6db
Ronald Robertson <gitronald@users.noreply.github.com>
2015-12-27 14:34:50 -0800

    reduced unnecessary code, made arguments consistent, added function examples, added documentation

--- R/demotables.R
+++ R/demotables.R
@@ -1,80 +1,85 @@
 # TODO:
-#  - Neatify numeric return on TRUE argument in dtable
 #  - Add sizesort argument to dtable
 #  - Move examples into roxygen comments
 
 #' detectClass
 #'
-#' Helper function for dtable. Obtains column classes and splits variable
-#' names into a list of two vectors based on numeric/integer or factor class.
+#' Helper function for \code{\link{dtable}}. Obtains column classes and splits
+#' variable names into a list of two vectors based on numeric/integer or factor
+#' class.
 #'
 #' @param data data.frame
-#' @param vnames vector of variable names from data.frame to classify
+#' @param vnames a vector of variable names from data.frame to classify
 #'
-#' @return Returns a list with a vector containing factor variable names,
-#'    and a vector containing numeric variable names.
-#' @seealso \code{\link{class}} which this function relies on
+#' @return Returns a list with a vector containing factor variable names, and a
+#'   vector containing numeric variable names.
+#' @seealso \code{\link{class}} to examine method for extracting class type.
 #' @export
 #' @examples
-#' data(demoData)
-#' detectClass(myData, "Education")
+#' # All variables
+#' detectClass(iris2, names(iris2))
 #'
-#' vars <- c("Education", "Age")
-#' detectClass(myData, vars)
-#'
+#' # Single variable
+#' detectClass(iris2, "Species")
 detectClass <- function(data, vnames){
+  detect <- cbind(vnames = vnames, dclass = NA)
+  for (i in 1:length(vnames)){
+    detect[, "dclass"][i] <- class(data[, vnames[i]])
+  }
 
-detect <- cbind(vnames = vnames, dclass = NA)
+  detected <- list()
+  detected[["factor"]] <- detect[detect[, "dclass"] == "factor", "vnames"]
+  detected[["numeric"]] <- detect[detect[, "dclass"] %in% c("numeric", "integer"), "vnames"]
 
-for (i in 1:length(vnames)){
-  detect[, "dclass"][i] <- class(data[, vnames[i]])
+  return(detected)
 }
 
-detected <- list()
-detected[["factor"]] <- detect[detect[, "dclass"] == "factor", "vnames"]
-detected[["numeric"]] <- detect[detect[, "dclass"] %in% c("numeric", "integer"), "vnames"]
-
-return(detected)
-}
-
-#' dtable
+#'dtable
 #'
-#' Demographic frequency tables and descriptive statistics tables for R.
+#'Demographic frequency tables and descriptive statistics tables for R.
 #'
 #' @param data data.frame
-#' @param vnames vector of variable names from data.frame to use in creation
-#'    of demographic tables
-#' @param round logical, \code{TRUE} returns rounded numeric table
-#' @param neat logical, \code{TRUE} returns rounded factor table with percent symbols
-#'
-#' @return list of data.frames $factor and $numeric if `vnames` contains both classes,
-#'    single data.frame returned if only one variable class in vnames.
-#'
+#' @param vnames a vector of variable names from \code{data.frame} to use in creation of
+#'   demographic tables
+#' @param neat logical, \code{TRUE} returns rounded factor table with percent
+#'   symbols
+#' @return List of two \code{data.frame}s, split into factor and numeric variables if
+#'   \code{vnames} contains both classes, single \code{data.frame} returned if only one
+#'   variable class detected in \code{vnames}.
+#' @importFrom psych describe
+#' @export
 #' @examples
-#' dtable(iris, names(iris))
+#' # Examine all variables
+#' dtable(iris2, names(iris2))
 #'
-dtable <- function (data, vnames, round = TRUE, neat = TRUE){
+#' # Examine single variable
+#' dtable(iris2, "Species")
+#'
+#' # Round all output and add percent symbols to factor output
+#' dtable(iris2, names(iris2), neat = TRUE)
+dtable <- function (data, vnames, neat = TRUE){
 
   detected <- detectClass(data, vnames)
-  # Produce frequencies table and descriptive stats table (dependent on available data)
   dtable <- list()
+
   if(length(detected$f) > 0) {
     dtable[["factor"]] <- do.call(rbind.data.frame, lapply(detected$f, dfactor, data = data, neat = neat))
     if(neat) {
-      dataset <- c(paste0(deparse(substitute(data))), rep("", nrow(dtable[["factor"]]) - 1))
+      dataset <- c(deparse(substitute(data)), rep("", nrow(dtable[["factor"]]) - 1))
     } else {
-      dataset <- rep(paste0(deparse(substitute(data))), nrow(dtable[["factor"]]))
+      dataset <- rep(deparse(substitute(data)), nrow(dtable[["factor"]]))
     }
     dtable[["factor"]] <- cbind(dataset, dtable[["factor"]])
     row.names(dtable[["factor"]]) <- NULL
   }
+
   if(length(detected$n) > 0) {
-    dtable[["numeric"]] <- do.call(rbind.data.frame, lapply(detected$n, dnumeric, data = data, round = round))
+    dtable[["numeric"]] <- do.call(rbind.data.frame, lapply(detected$n, dnumeric, data = data, neat = neat))
     dtable[["numeric"]] <- dtable[["numeric"]][, -1]
     if(neat) {
-      dataset <- c(paste0(deparse(substitute(data))), rep("", nrow(dtable[["numeric"]]) - 1))
+      dataset <- c(deparse(substitute(data)), rep("", nrow(dtable[["numeric"]]) - 1))
     } else {
-      dataset <- rep(paste0(deparse(substitute(data))), nrow(dtable[["numeric"]]))
+      dataset <- rep(deparse(substitute(data)), nrow(dtable[["numeric"]]))
     }
     dtable[["numeric"]] <- cbind(dataset, dtable[["numeric"]])
     row.names(dtable[["numeric"]]) <- NULL
@@ -85,17 +90,24 @@ dtable <- function (data, vnames, round = TRUE, neat = TRUE){
 
 #' dfactor
 #'
-#' This function converts a column(s) from a data.frame into a frequencies table
+#' This function converts a column(s) from a \code{data.frame} into a frequencies table
 #' formatted in standard presentation structure with percent symbols.
 #'
 #' @param data a \code{data.frame}
-#' @param vnames vector of variable names from \code{data.frame} to use in creation
-#'    of demographics table, multiple names produces crosstabs.
-#' @param neat logical, \code{TRUE} returns rounded factor table with percent symbols
+#' @param vnames a vector of variable names from \code{data.frame} to use in
+#'   creation of demographics table, multiple variables produces more specific
+#'   groups.
+#' @param neat logical, \code{TRUE} returns rounded factor table with percent
+#'   symbols
 #' @param sizesort logical, \code{TRUE} returns table sorted by size
+#' @return Returns a demographic frequency table of varying specificity.
+#' @export
+#' @examples
+#' # Single demographic
+#' dfactor(iris2, "Species")
 #'
-#' @return data.frame
-#'
+#' # Two demographics
+#' dfactor(iris2, c("Color", "Species"))
 dfactor <- function (data, vnames, neat = TRUE, sizesort = TRUE) {
 
   # First column - Name the demographic from object name
@@ -122,7 +134,7 @@ dfactor <- function (data, vnames, neat = TRUE, sizesort = TRUE) {
 
     # If only one variable, remove repetitive demographic IDs (presentation format)
     if (length(vnames) == 1) {
-      Demographic <- c(DemoName, rep("", (length(table(data[vnames]))-1)))
+      Demographic <- c(DemoName, rep("", (length(table(data[vnames])) - 1)))
     }
   }
 
@@ -146,24 +158,33 @@ dfactor <- function (data, vnames, neat = TRUE, sizesort = TRUE) {
 
 #' dnumeric
 #'
-#' This function converts a numeric column from a data.frame into a descriptive statistics
-#' table using \code{psych::describe}.
+#' Wrapper for \code{psych::describe} that adds the name of the dataset and the
+#' variable being examined to its output. Use in \code{dtable} for multiple
+#' variables.
 #'
-#' @param data data.frame
-#' @param vnames single variable name to examine with \code{psych::describe}
-#' @param round logical, returns rounded values if \code{TRUE}
+#' @param data a \code{data.frame}
+#' @param vnames a single variable name to examine with \code{psych::describe}
+#' @param neat logical, returns rounded values if \code{TRUE}
+#' @seealso See \code{\link{dtable}}
+#' @seealso See \code{\link[psych]{describe}} for details on the descriptives
+#'   returned
+#' @return Returns a \code{data.frame} with common descriptive statistics for a
+#'   numeric variable, as defined in \code{psych::describe}, concatenated with
+#'   the name of the dataset and the name of the variable.
+#' @importFrom psych describe
+#' @export
+#' @examples
+#' # Single variable
+#' dnumeric(iris2, "Sepal.Length")
 #'
-#' @return data.frame
-#'
-dnumeric <- function(data, vnames, round = FALSE) {
-
-    require(psych)
+#' # Use dtable for multiple variables
+dnumeric <- function(data, vnames, neat = FALSE) {
   dataset  <- deparse(substitute(data))
   variable <- paste0(vnames)
   descript <- describe(data[, vnames])
   results  <- cbind(dataset, variable, descript)
 
-  if (round) {
+  if (neat) {
     results <- cbind(results[, 1:4], round(results[, 5:length(results)], digits = 1))
   }
 
