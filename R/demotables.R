f008ca1f9aa04c0939fb9bdf2cd0b440fab3f27d
Ronald Robertson <gitronald@users.noreply.github.com>
2016-01-16 22:01:54 -0800

    Move dvariable function family to new script

--- R/demotables.R
+++ R/demotables.R
@@ -1,72 +1,3 @@
-#' Data.frame of Variables and Classes
-#'
-#' Helper function for \code{\link{dtable}}. Returns information about variable
-#' class in a \code{data.frame}, or \code{list} if \code{as.list = TRUE}.
-#'
-#' @param data1 a \code{data.frame}
-#' @return Returns a data.frame or list with the variable names and their
-#'   respective classes. Returns a data.frame by default.
-#' @seealso \code{\link{class}} to examine method for extracting class.
-#' @export
-#' @examples
-#' # Load sample data
-#' dclass(iris2)
-#'
-#' # Return variable and class data in data.frame
-#' dclass(iris2)
-#'
-#' # Return variable and class data in list
-#' dclass(iris2, as.list = TRUE)
-dclass <- function(data1, as.list = FALSE){
-  data1 <- lapply(data1, class)                          # Collect data
-  data1 <- data.frame((unlist(data1)))                   # Shape data
-  data1 <- pull_rownames(data1)                          # Pull in row names
-  data1 <- setNames(data1, c("variable", "class"))       # Set column names
-  if (as.list) data1 <- split(data1, data1[["class"]])   # If, convert to list
-
-  return(data1)
-}
-
-dmode <- function(data1, as.list = FALSE){
-  data1 <- lapply(data1, mode)                           # Collect data
-  data1 <- data.frame((unlist(data1)))                   # Shape data
-  data1 <- pull_rownames(data1)                          # Pull in row names
-  data1 <- setNames(data1, c("variable", "mode"))        # Set column names
-  if (as.list) data1 <- split(data1, data1[["mode"]])    # If, convert to list
-
-  return(data1)
-}
-
-dtypeof <- function(data1, as.list = FALSE){
-  data1 <- lapply(data1, typeof)                        # Collect data
-  data1 <- data.frame((unlist(data1)))                  # Shape data
-  data1 <- pull_rownames(data1)                         # Pull in row names
-  data1 <- setNames(data1, c("variable", "typeof"))     # Set column names
-  if (as.list) data1 <- split(data1, data1[["typeof"]]) # If, convert to list
-
-  return(data1)
-}
-
-dvariable <- function(data1){
-  if (!("data.frame" %in% class(data1))) {
-    stop("Input 'data1' must be of class data.frame")
-  }
-  variable <- names(data1)
-  class    <- dclass(data1)[, 2]
-  mode     <- dmode(data1)[, 2]
-  type     <- dtypeof(data1)[, 2]
-  data2 <- data.frame(variable, class, mode, type)
-
-  return(data2)
-}
-
-pull_rownames <- function(data1){
-  data1 <- data.frame(row.names(data1), data1)
-  row.names(data1) <- NULL
-  return(data1)
-}
-
-
 #' Generate demographic frequencies and descriptive statistics tables
 #'
 #' Generate demographic frequencies tables and descriptive statistics tables in
@@ -106,11 +37,12 @@ dtable <- function (data, vnames = NULL, neat = TRUE, sizesort = TRUE){
 
   if (is.null(vnames)) vnames <- names(data1)  # Default selection to all variables
 
-  class.data  <- dclass(data, as.list = TRUE)
+  col.data  <- dvariable(data)
+  col.data  <- split(col.data, col.data[["type"]])
   dtable <- list()
 
-  if(length(type[["factor"]]) > 0) {
-    dtable[["factor"]] <- do.call(rbind.data.frame, lapply(class.data$factor$variable, dfactor,
+  if(length(col.data[["character"]]) > 0) {
+    dtable[["factor"]] <- do.call(rbind.data.frame, lapply(col.data[["character"]][["variable"]], dfactor,
                                                            data = data, neat = neat,
                                                            sizesort = sizesort))
     if(neat) {
@@ -118,6 +50,7 @@ dtable <- function (data, vnames = NULL, neat = TRUE, sizesort = TRUE){
     } else {
       dataset <- rep(deparse(substitute(data)), nrow(dtable[["factor"]]))
     }
+
     dtable[["factor"]] <- cbind(dataset, dtable[["factor"]])
     row.names(dtable[["factor"]]) <- NULL
   }
