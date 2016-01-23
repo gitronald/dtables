d7efe9a8788c5dcb5ebf159a125080085eb041ca
Ronald Robertson <gitronald@users.noreply.github.com>
2016-01-23 09:30:33 -0800

    Add date and num int vars to data

--- data-raw/iris2.R
+++ data-raw/iris2.R
@@ -6,6 +6,10 @@ iris2[["Color"]] <- sample(c("orange", "blue", "red", "yellow"), 150, replace = 
 iris2[["Attractiveness"]] <- sample(1:5, 150, replace = T)                          # Add ordered factor
 iris2[["LikelyToBuy"]] <- sample(-5:5, 150, replace = T)                            # Add ordered factor
 iris2[["Sold"]] <- sample(c(TRUE, FALSE), 150, replace = T)                         # Add logical
+iris2[["Review"]] <- as.numeric(sample(1:5, 150, replace = T))                      # Add numeric integer
+iris2[["Date"]] <- sample(seq.Date(as.Date("2015-09-01"), Sys.Date(), "day"),       # Add date
+                          150, replace = T)
+
 row.names(iris2) <- NULL
 
 devtools::use_data(iris2, overwrite = T)
