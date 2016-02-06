0c1e4720a9aeb6d66d64ca86ba5e3dec0c14e610
Ronald Robertson <gitronald@users.noreply.github.com>
2016-02-06 14:02:52 -0800

    Remove old examples

--- R/dvariables.R
+++ R/dvariables.R
@@ -13,11 +13,11 @@
 #'
 #' @export
 #' @examples
-#' # Load sample data
+#' # Describe all variables in iris2
 #' dvariable(iris2)
 #'
-#' # Return variable and class data in data.frame
-#' dvariable(iris2)
+#' # Describe one or more specific variables in iris2
+#' dvariable(iris2, vars = "Species")
 #'
 #' # Return variable and class data in list
 #' dvariable(iris2, as.list = TRUE)
