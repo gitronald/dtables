a15f9cc707f9842a270fd416a13aad466fc954df
Ronald Robertson <gitronald@users.noreply.github.com>
2016-02-13 03:28:35 -0800

    Reduce to one function, add options

--- R/data_frame_table.R
+++ R/data_frame_table.R
@@ -1,12 +1,25 @@
-data_frame_table <- function(data1){
-  t   <- table(data1)
-  dft <- data.frame(t)
+#' Create a data.frame table
+#'
+#' Create a data.frame table with an optional proportion column
+#'
+#' @param data1 a vector or data.frame column
+#' @param prop logical, if \code{TRUE} returns an additional proportion column
+#'
+#' @return a data.frame table with an optional proportion column
+#' @export
+#'
+#' @examples
+#' data_frame_table(iris2$Species)
+#' data_frame_table(iris2$Species, prop = FALSE)
+data_frame_table <- function(data1, prop = TRUE){
+  t    <- table(data1)
+  prop <- table_prop(t)
+  dft  <- data.frame(t, prop)
+  names(dft) <- c("group", "n", "prop")
   return(dft)
 }
 
-data_frame_prop_table <- function(data1){
-  t   <- table(data1)
-  prop <- as.vector(t/sum(t))
-  dft <- data.frame(t, p)
-  return(dft)
+# data_frame_table helper function - proportions
+table_prop <- function(table){
+  table.prop <- as.vector(table)/sum(table)
 }
