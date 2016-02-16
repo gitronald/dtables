81f93adab6003e6b0f16b39dd2486686b0090214
Ronald Robertson <gitronald@users.noreply.github.com>
2016-02-15 23:48:39 -0800

    Implement data_frame_table in dfactor and advnace version

--- R/dfactor.R
+++ R/dfactor.R
@@ -25,29 +25,7 @@ dfactor <- function (data1, vars, neat = TRUE, sizesort = TRUE) {
   name        <- paste0(vars)
   Demographic <- rep(name, n)
 
-  # Second & Third columns - Demographic factors and frequency counts
-  dgroup <- table(data1[, vars])
-  dgroup <- data.frame(dgroup)
-
-  # Fourth column - Percent value of frequency count
-  dgroup <- transform(dgroup, Prop = prop.table(dgroup[, "Freq"]))
-
-  # Make table neat by rounding and removing repetitive labels
-  if (neat) {
-    # Convert proportion to percent
-    dgroup[, "Perc"] <- dgroup[, "Prop"]*100
-    dgroup[, "Perc"] <- format(round(dgroup[, "Perc"], 1), nsmall = 1)
-    dgroup[, "Prop"] <- NULL
-
-    # Add percent symbol: "$" is end of string in regex
-    dgroup[, "Perc"] <- gsub("$", "%", dgroup[, "Perc"])
-
-    # If only one variable, remove repetitive demographic IDs (presentation format)
-    if (length(vars) == 1) {
-      Dataset <- c(Dataset[1], rep("", (n - 1)))
-      Demographic <- c(name, rep("", (n - 1)))
-    }
-  }
+  dgroup <- data_frame_table(data1[, vars])
 
   # Rename demographic group variable and demographic ID if more than 1 used
   if (length(vars) == 1) {
@@ -66,3 +44,7 @@ dfactor <- function (data1, vars, neat = TRUE, sizesort = TRUE) {
 
   return(dgroup)
 }
+
+
+
+
