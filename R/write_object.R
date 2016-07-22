721a856f2c1cffb479a695aa349b99ecd2c687b6
gitronald <gitronald@users.noreply.github.com>
2016-07-22 14:46:58 -0700

    Fix write object

--- R/write_object.R
+++ R/write_object.R
@@ -9,6 +9,7 @@
 #'   the object, defaults to current working directory.
 #' @param date logical, if \code{TRUE} appends the object name with todays date
 #' @param overwrite logical, if \code{TRUE} overwrites existing file of same name
+#' @param row.names logical, if \code{TRUE} adds rownames to output file
 #' @return Returns nothing. Saves "object_date.tsv" or "object.tsv" to current
 #'   directory.
 #' @importFrom utils write.table
@@ -16,25 +17,30 @@
 #' @examples
 #' write_object(iris2)
 #'
-write_object <- function(object, dir = NULL, date = TRUE, overwrite = FALSE) {
+write_object <- function(object, dir = NULL, date = TRUE, overwrite = FALSE, row.names = FALSE) {
   if(date){
     date <- paste0("_", Sys.Date())
   } else {
     date <- ""
   }
-  object <- deparse(substitute(object))
+  name <- deparse(substitute(object))
   if(!is.null(dir)) {
-    file.name <- paste0(dir, "/", object, date, ".tsv")
+    file.name <- paste0(dir, "/", name, date, ".tsv")
     if(file.name %in% list.files(recursive = T) && overwrite == FALSE) {
       stop(paste0("File '", file.name, "' already exists"))
     }
   } else {
-    file.name <- paste0(object, date, ".tsv")
+    file.name <- paste0(name, date, ".tsv")
     if(file.name %in% list.files() && overwrite == FALSE) {
       stop(paste0("File '", file.name, "' already exists"))
     }
   }
+
+  col.names = ifelse(row.names, NA, TRUE)
+
   write.table(object,
               file = file.name,
-              sep = "\t")
+              sep = "\t",
+              row.names = row.names,
+              col.names = col.names)
 }
